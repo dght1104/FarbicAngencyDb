43617c5e2e828625071b1077615ec6c12b3009ea
dghant <152829011+giahan3@users.noreply.github.com>
2023-12-06 21:59:56 +0700

    Add files via upload

---
+++ pages/btndemo.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="../css/button.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<button class="Btn" name="btnsubmit" onclick="alert('Học Code Tốt Xin chào các bạn')">
+    <span class="btnIcon">
+        <i class="fa fa-plus"></i>
+    </span>
+    <span class="text">Copy</span> 
+</button>
